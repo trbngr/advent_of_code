318001dc1ef18cd061c6c345613a0eaada9afb8c
Chris Martin <chris.martin@oforce.com>
2021-12-05 17:14:02 -0700

    simplify

--- lib/2021/5.ex
+++ lib/2021/5.ex
@@ -1,21 +1,11 @@
 defmodule Y2021.D5 do
   use AOC, day: 5, year: 2021
 
-  def p1 do
-    p1(input_stream() |> Enum.to_list())
-  end
+  def p1, do: p1(input_stream() |> Enum.to_list())
+  def p1(input), do: run_input(input)
 
-  def p1(input) do
-    run_input(input)
-  end
-
-  def p2 do
-    p2(input_stream() |> Enum.to_list())
-  end
-
-  def p2(input) do
-    run_input(input, :inc_diagonals)
-  end
+  def p2, do: p2(input_stream() |> Enum.to_list())
+  def p2(input), do: run_input(input, :inc_diagonals)
 
   defp run_input(input, opt \\ nil) do
     input
@@ -24,13 +14,11 @@ defmodule Y2021.D5 do
     |> Enum.count(&(elem(&1, 1) > 1))
   end
 
-  defp mark_line([{x1, y}, {x2, y}], grid, _opt) do
-    Enum.reduce(x1..x2, grid, &mark_point({&1, y}, &2))
-  end
+  defp mark_line([{x1, y}, {x2, y}], grid, _opt),
+    do: Enum.reduce(x1..x2, grid, &mark_point({&1, y}, &2))
 
-  defp mark_line([{x, y1}, {x, y2}], grid, _opt) do
-    Enum.reduce(y1..y2, grid, &mark_point({x, &1}, &2))
-  end
+  defp mark_line([{x, y1}, {x, y2}], grid, _opt),
+    do: Enum.reduce(y1..y2, grid, &mark_point({x, &1}, &2))
 
   defp mark_line([{x1, y1}, {x2, y2}], grid, :inc_diagonals) do
     [Enum.map(x1..x2, & &1), Enum.map(y1..y2, & &1)]
